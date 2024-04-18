fb15a3b72083363f2433a7b3556185125de20560
Chiwuikem1 <123129260+Chiwuikem@users.noreply.github.com>
2024-04-18 16:07:47 -0400

    to dev branch

--- prompts.py
+++ prompts.py
@@ -5,4 +5,15 @@ Created on Thu Apr  4 13:08:19 2024
 
 @author: stevehalley
 """
+def ask_setting():
+    return input("Where did the dream take place? (e.g., city, house, beach): ")
+
+def ask_characters():
+    return input("Who were the characters in the dream? Describe them: ")
+
+def ask_emotions():
+    return input("What emotions did you experience in the dream? Describe your feelings: ")
+
+def ask_actions():
+    return input("What were you doing in the dream? Describe your actions: ")
 
