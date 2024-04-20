1e06264c9339120b148ae80d5dff7c8b1da0ccf8
Chiwuikem1 <123129260+Chiwuikem@users.noreply.github.com>
2024-04-20 16:26:50 -0400

    Updated prompts pt.2

--- prompts.py
+++ prompts.py
@@ -5,15 +5,22 @@ Created on Thu Apr  4 13:08:19 2024
 
 @author: stevehalley
 """
-def ask_setting():
-    return input("Where did the dream take place? (e.g., city, house, beach): ")
+class Prompts:     
+    def ask_setting():
+        return input("Where did the dream take place? ")
+    def ask_characters():
+        return input("Who were the characters in the dream? Was there anyone that you liked or disliked?")
 
-def ask_characters():
-    return input("Who were the characters in the dream? Describe them: ")
+    def ask_emotions():
+        return input("What emotions did you experience in the dream or when you woke up? ")
 
-def ask_emotions():
-    return input("What emotions did you experience in the dream? Describe your feelings: ")
-
-def ask_actions():
-    return input("What were you doing in the dream? Describe your actions: ")
-
+    def ask_actions():
+        return input("What were you doing in the dream?")
+    
+    def ask_symbols():
+        return input("Where there and objects or symbols that stood out to you?")
+    
+    def ask_experiences():
+        return input("Was the dream familiar or related to any real life experiences?")
+    
+    
