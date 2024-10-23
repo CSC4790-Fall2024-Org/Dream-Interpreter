58dc3497ccf4e011653cc0bf8a9dd431752e68cf
Sam <119359217+srken32@users.noreply.github.com>
2024-10-23 12:31:20 -0400

    authentication files and npm

--- ReactNative/dream-app/app/_layout.tsx
+++ ReactNative/dream-app/app/_layout.tsx
@@ -2,10 +2,10 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ title: "Home" }} />
-      <Stack.Screen name="gemini" options={{ title: "Gemini Interpretation" }} />
-      <Stack.Screen name="gooey" options={{ title: "Gooey Animation" }} />
-    </Stack>
+      <Stack>
+        <Stack.Screen name="index" options={{ title: "Home" }} />
+        <Stack.Screen name="gemini" options={{ title: "Gemini Interpretation" }} />
+        <Stack.Screen name="gooey" options={{ title: "Gooey Animation" }} />
+      </Stack>
   );
 }
