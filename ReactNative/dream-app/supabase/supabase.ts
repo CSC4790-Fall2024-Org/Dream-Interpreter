bfa49919810fa76608b25feba24f9a86e5a70b4e
Sam <119359217+srken32@users.noreply.github.com>
2024-11-17 15:33:45 -0500

    Remove user-management template

--- ReactNative/dream-app/supabase/supabase.ts
+++ ReactNative/dream-app/supabase/supabase.ts
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://slaxbnfmvswlyrzuydde.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsYXhibmZtdnN3bHlyenV5ZGRlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk3MDEwNzYsImV4cCI6MjA0NTI3NzA3Nn0.EjRS55ougX6mW97cvvcvdq3Z0MZBwsVH0qhjGmFxDNI';
+const supabaseUrl = 'https://zxqxxhnrngrlanrrcfvk.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4cXh4aG5ybmdybGFucnJjZnZrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjk4NzMzNTksImV4cCI6MjA0NTQ0OTM1OX0.ToSvWc_Zp5N-Ab4Q9ZD08dnsUsLOe00-7Hh6OfGuNzY';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
