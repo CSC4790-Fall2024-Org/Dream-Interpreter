cb1e545f31d23a8b9c9d8836221e65aced453fee
Yasmin Lorin Kaygalak <ykaygala@villanova.edu>
2024-12-05 15:08:42 -0500

    Small UI fixes

--- ReactNative/dream-app/app/_layout.tsx
+++ ReactNative/dream-app/app/_layout.tsx
@@ -3,9 +3,9 @@ import { Stack } from "expo-router";
 export default function RootLayout() {
   return (
       <Stack>
-        <Stack.Screen name="index" options={{ title: " 🌙Nocturnal Navigator 🌙" }} />
-        <Stack.Screen name="gemini" options={{ title: "Gemini Interpretation" }} />
-        <Stack.Screen name="gooey" options={{ title: "Gooey Animation" }} />
+        <Stack.Screen name="index" options={{ title: "💫Nocturnal Navigator💫" }} />
+        <Stack.Screen name="gemini" options={{ title: "💫Textual Interpretation💫" }} />
+        <Stack.Screen name="gooey" options={{ title: "💫Visual Animation💫" }} />
       </Stack>
   );
 }
